c67551c5649f9d1e186965bc867360ab8d61a026
Specific Repo User <repo_email@example.com>
2025-12-05 13:58:45 -0500

    first commit

---
+++ home-cafe/src/App.js
@@ -0,0 +1,15 @@
+
+import { AppContent } from './components/AppContent';
+import { AppHeader } from "./components/AppHeader"
+
+function App() {
+  return (
+    <>
+      <AppHeader />
+      <AppContent />
+    </>
+   
+  );
+}
+
+export default App;
