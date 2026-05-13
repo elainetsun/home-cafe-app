80a0600bb28c526638deaba2c491d946a3ab8f3f
elainetsun <elaine.tsun3@gmail.com>
2026-05-13 12:16:01 -0400

    fix url

--- home-cafe/src/constants.js
+++ home-cafe/src/constants.js
@@ -1,7 +1,7 @@
 export const APP_HEADER_TEXT = "Snuffles Cafe";
 export const APP_HEADER_SUBTEXT = "yesssss what would you like?";
 
-export const BASE_URL = "http://snuffles.cafe";
+export const BASE_URL = "https://snuffles.cafe";
 
 // export const MENU_ITEMS_URL = `${process.env.REACT_APP_API_BASE_URL}/menuItems`;
 // export const ORDER_ITEM_URL = `${process.env.REACT_APP_API_BASE_URL}/orders`;
